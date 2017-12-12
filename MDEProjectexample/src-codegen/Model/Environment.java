72ba3e8b607e95e2a310a554881a540d17a2b12d
nimishkr <nimish97@yahoo.co.in>
2017-12-12 19:29:53 +0100

    Made new class Environment that can be used to make new environments.

---
+++ MDEProjectexample/src-codegen/Model/Environment.java
@@ -0,0 +1,15 @@
+package Model;
+
+import simbad.sim.EnvironmentDescription;
+
+public class Environment extends EnvironmentDescription {
+	
+	public Environment(ArrayList<Areas> areas) {
+		super();
+	}
+	
+	public void getAreas() {
+		this.
+	}
+
+}
